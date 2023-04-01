0906eb1136c15477242ed5e6325a6f028a1a3e8e
Sri Janani Ramesh <k97a5@unb.ca>
2023-04-01 15:47:15 -0300

    made changes

--- script1.js
+++ script1.js
@@ -1,11 +1,24 @@
 const form = document.querySelector('form');
 const nameInput = document.querySelector('#name');
-const submitLink = document.querySelector('#submit-link');
+const submitButton = document.querySelector('#submit-link');
 
-submitLink.addEventListener('click', (e) => {
-    e.preventDefault();
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
     const name = nameInput.value;
     console.log(`Hello, ${name}!`);
-    // Navigate to the next page with the user's input
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "storename.php");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(`name=${name}`);
+
+ 
     window.location.href = "HTMLpg.html";
 });
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    form.submit(); 
+});
+
+
---
+++ storename.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $file = "usernames.txt"; // Name of the file where you want to store the names
+
+  // Open the file in append mode
+  $fp = fopen($file, "a");
+
+  // Write the name to the file
+  fwrite($fp, $name . "\n");
+
+  // Close the file
+  fclose($fp);
+}
+?>
